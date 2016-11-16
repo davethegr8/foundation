539a1c77b0c9f03d0a6669b45809d22b1ae5b3e8
Dave Poole <david.poole@bigfishgames.com>
2016-11-15 16:40:15 -0800

    add debug function

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -9,3 +9,9 @@ function any() {
     foreach(func_get_args() as $arg) if(boolval($arg)) return true;
     return false;
 }
+
+function debug() {
+    echo '<pre>';
+    foreach(func_get_args() as $arg) print_r($arg);
+    echo '</pre>';
+}
