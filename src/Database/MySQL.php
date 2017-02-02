542969af6f6e6f477720418b3ad919dd8a49407c
Dave <davethegr8@users.noreply.github.com>
2017-02-02 07:27:35 -0800

    use utf8

--- src/Database/MySQL.php
+++ src/Database/MySQL.php
@@ -7,7 +7,7 @@ use Hep\Foundation\Database;
 class MySQL extends Database {
 
     public function __construct($host, $user = '', $pass = '', $name = '', array $options = []) {
-        $dsn = 'mysql:host='.$host.';dbname='.$name;
+        $dsn = 'mysql:host='.$host.';dbname='.$name.';charset=UTF8';
         parent::__construct($dsn, $user, $pass, $options);
     }
 
