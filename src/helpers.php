941650aeb0cfe79ce1d1ea019d5b277c4101eb8d
Dave Poole <davethegr8@gmail.com>
2017-06-14 14:42:14 -0700

    move file functions back to helpers

--- src/helpers.php
+++ src/helpers.php
@@ -169,3 +169,20 @@ function starts_with($haystack, $needle) {
 function ends_with($haystack, $needle) {
     return Str::ends_with($haystack, $needle);
 }
+
+
+function file_extension($filename) {
+    return File::extension($filename);
+}
+
+function file_name($filename) {
+    return File::name($filename);
+}
+
+function file_upload($file, $destination) {
+    return File::upload($file, $destination);
+}
+
+function format_filesize($size) {
+    return File::formatSize($size);
+}
