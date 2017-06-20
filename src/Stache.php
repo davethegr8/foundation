a8a6d3259d4cd1398d6da9b7d6844d3d52f07ae6
Dave Poole <david.poole@bigfishgames.com>
2017-06-20 13:35:01 -0700

    stub for stache

--- src/Stache.php
+++ src/Stache.php
@@ -5,4 +5,22 @@ namespace Hep\Foundation;
 // it's like mustache... but smaller
 class Stache {
 
+    // filters must have a single input and produce a single output
+    // if you need something with more inputs, use currying
+    protected $allowFilters = true;
+    protected $forbiddenFunctions = ['exec', 'shell_exec', 'passthru'];
+
+    public function __construct() {}
+
+    public function allowFilters($value = null) {
+        if(is_bool($value)) {
+            $this->allowFilters = $value;
+        }
+
+        return $this->allowFilters;
+    }
+
+    // context should allow for multilevel arrays: { parent.child }
+    public function render($template, $context, $replaceEmpty = false) {}
+
 }
