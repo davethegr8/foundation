4b925e552b2734f13ed9b378a33f294ffda17ccd
Dave Poole <davethegr8@gmail.com>
2017-06-21 04:21:07 -0700

    add asserts

--- test/run.php
+++ test/run.php
@@ -5,12 +5,20 @@ include '../vendor/autoload.php';
 use Hep\Foundation\Stache;
 
 $engine = new Stache();
-// $engine->allowTransforms(false);
 
-echo $engine->render('{ item.link }', [
+assert($engine->render('{ empty }', []) === '{ empty }');
+assert($engine->render('{ empty }', [], true) === '');
+
+assert($engine->render('{ test }', ['test' => 'SUCCESS']) === 'SUCCESS');
+assert($engine->render('{test | exec | passthru | shell_exec | system }', ['test' => 'SUCCESS']) === 'SUCCESS');
+assert($engine->render('{test | strtolower}', ['test' => 'SUCCESS']) === 'success');
+
+assert($engine->render('{ item.link }', [
     'item' => [
-        'value' => 1,
-        'link' => 'asdf'
+        'link' => 'success'
     ]
-]);
-echo $engine->render('{ test }{test | strtolower}', ['test' => 'SUCCESS']);
+]) === 'success');
+
+$engine->allowTransforms(false);
+assert($engine->render('{test | strtolower}', ['test' => 'SUCCESS']) === 'SUCCESS');
+
